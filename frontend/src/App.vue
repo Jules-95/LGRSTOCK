f2a6f93448fdf885020a675e516c4f306fd5ddfb
Jules-95 <jlsmillet@yahoo.fr>
2026-04-10 14:19:55 +0200

    fix(app): call initi() in App.vue to restore user Session on refresh #30

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,13 @@
   </div>
 </template>
 
+<script setup>
+import { useAuth } from './composables/useAuth';
+
+const { init } = useAuth()
+init()
+</script>
+
 <style>
 /* Importation du CSS global */
 @import './assets/main.css';
